b602d66c8b587e7c2ff67df70cf618c2ebf6d82a
Will O'Brien <will.obrien@dibit.co>
2015-08-03 06:40:57 -0400

    version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject units "0.1.0-SNAPSHOT"
-  :description "Unit conversions in clojure"
+(defproject io.ramekin/units "0.1.0"
+  :description "Unit conversion in clojure"
   :url "https://github.com/ramekin-io/units"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
